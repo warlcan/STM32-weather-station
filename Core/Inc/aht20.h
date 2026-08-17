3a9b2d016bed480f84ee8a7f283fa416f94bafd6
warlcan <walzes@yandex.ru>
2026-08-18 01:04:53 +0300

    refactor: use define for AHT20 I2C bus

--- Core/Inc/aht20.h
+++ Core/Inc/aht20.h
@@ -6,6 +6,7 @@
 #include "stm32l0xx_ll_i2c.h"
 #include "i2c_utils.h"
 
+#define AHT20_I2C I2C1
 #define AHT20_I2C_ADDRESS 0x38 << 1
 
 #define AHT20_CALIBRATE_DELAY_MS 40
@@ -19,6 +20,6 @@ typedef struct {
     float temperature;
 } AHT20_Data_t;
 
-bool aht20_get_data(I2C_TypeDef *I2Cx, AHT20_Data_t *out_data);
+bool aht20_get_data(AHT20_Data_t *out_data);
 
 #endif
--- Core/Src/aht20.c
+++ Core/Src/aht20.c
@@ -2,26 +2,26 @@
 
 extern void LowPower_Delay(uint32_t Delay); 
 
-bool aht20_get_data(I2C_TypeDef *I2Cx, AHT20_Data_t *out_data) {
+bool aht20_get_data(AHT20_Data_t *out_data) {
     //Clear flags
-    LL_I2C_ClearFlag_NACK(I2Cx);
-    LL_I2C_ClearFlag_BERR(I2Cx);
+    LL_I2C_ClearFlag_NACK(AHT20_I2C);
+    LL_I2C_ClearFlag_BERR(AHT20_I2C);
 
     //Transmit
     uint8_t measure_cmd_bytes[3] = {0xAC, 0x33, 0x00};
-    if(!transmit_command(I2Cx, AHT20_I2C_ADDRESS, measure_cmd_bytes, 3)) return false;
+    if(!transmit_command(AHT20_I2C, AHT20_I2C_ADDRESS, measure_cmd_bytes, 3)) return false;
 
     LowPower_Delay(AHT20_MEASURE_DELAY_MS);
     
     //Receive
     uint8_t receive_data_buffer[6];
-    if(!receive_data(I2Cx, AHT20_I2C_ADDRESS, receive_data_buffer, 6)) return false;
+    if(!receive_data(AHT20_I2C, AHT20_I2C_ADDRESS, receive_data_buffer, 6)) return false;
 
     //Check errors
     if ((receive_data_buffer[0] & AHT20_STATUS_BUSY_BIT) != 0) return false;
     if ((receive_data_buffer[0] & AHT20_STATUS_CAL_BIT)  == 0) {
         uint8_t calibrate_cmd_bytes[] = {0xBE, 0x08, 0x00};
-        if(!transmit_command(I2Cx, AHT20_I2C_ADDRESS, calibrate_cmd_bytes, 3)) return false;
+        if(!transmit_command(AHT20_I2C, AHT20_I2C_ADDRESS, calibrate_cmd_bytes, 3)) return false;
         LowPower_Delay(AHT20_CALIBRATE_DELAY_MS);
         return false;
     }
