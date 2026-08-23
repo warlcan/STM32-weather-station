a1eba7da2bdc91bd4a76a4f801a0b6948b7b7aee
warlcan <walzes@yandex.ru>
2026-08-23 03:07:18 +0300

    feat: implementate of interfaces aht20, bmp280, nrf24

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -115,6 +115,9 @@ int main(void)
   /* USER CODE BEGIN 2 */
   DEBUG_RTT_Init();
   DEBUG_RTT_WriteString(0, "RTT initialized.\r\n");
+
+  nrf24_init();
+  bmp280_init();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -124,7 +127,30 @@ int main(void)
   /* USER CODE END WHILE */
   
   /* USER CODE BEGIN 3 */
+  if(!aht20_get_data(&aht20_data)) {
+    DEBUG_RTT_WriteString(0, "aht20 error");
+    continue;
+  }
+  if(!bmp280_get_data(&bmp280_data)) {
+    DEBUG_RTT_WriteString(0, "bmp280 error");
+    continue;
+  }
+  
+  nrf24_data.temperature = aht20_data.temperature;
+  nrf24_data.humidity    = aht20_data.humidity;
+  nrf24_data.pressure    = bmp280_data.pressure;
 
+  DEBUG_RTT_WriteInt(nrf24_data.temperature); DEBUG_RTT_PutChar(0, '\n');
+  DEBUG_RTT_WriteInt(nrf24_data.humidity); DEBUG_RTT_PutChar(0, '\n');
+  DEBUG_RTT_WriteInt(nrf24_data.pressure); DEBUG_RTT_PutChar(0, '\n');
+
+  if(!nrf24_transmit_data(&nrf24_data)) {
+    DEBUG_RTT_WriteString(0, "nrf24 error\n");
+    continue;
+  }
+  DEBUG_RTT_WriteString(0, "nrf24 packet send\n");
+
+  LL_mDelay(5000);
   /* USER CODE END 3 */
   }
 }
